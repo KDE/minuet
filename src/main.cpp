14c969e1edf6869895a5e30802e741f6c9b44077
Sandro S. Andrade <sandroandrade@kde.org>
2016-03-28 13:23:39 -0300

    Fix wrong translation of exercise instructions

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,8 @@ int main(int argc, char **argv)
                           KAboutLicense::GPL,
                           i18n("(c) 2016, Sandro S. Andrade (sandroandrade@kde.org)"));
 
-    aboutData.addAuthor(i18n("Sandro S. Andrade"),i18n("Author"), QStringLiteral("sandroandrade@kde.org"));
+    aboutData.addAuthor(i18n("Sandro S. Andrade"),i18n("Developer"), QStringLiteral("alessandro.longo@kdemail.net"));
+    aboutData.addAuthor(i18n("Alessandro Longo"),i18n("Minuet Icon Designer"), QStringLiteral("sandroandrade@kde.org"));
     application.setWindowIcon(QIcon::fromTheme(QStringLiteral("minuet")));
     QCommandLineParser parser;
     parser.addHelpOption();
