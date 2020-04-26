550d607667d7ebdc48f5f1d7c1793b85bd561dba
Patrick José Pereira <patrickelectric@gmail.com>
2020-04-25 21:21:20 -0300

    Add utils namespace
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ src/utils/xdgdatadirs.cpp
@@ -0,0 +1,25 @@
+#include <QDir>
+#include <QFile>
+
+#include "xdgdatadirs.h"
+
+using namespace Utils;
+
+QStringList Utils::getXdgDataDirs() {
+    const QString xdgDataDirsEnv = QFile::decodeName(qgetenv("XDG_DATA_DIRS"));
+    if (xdgDataDirsEnv.isEmpty()) {
+        return {};
+    }
+
+    QStringList results;
+    const auto paths = xdgDataDirsEnv.splitRef(QLatin1Char(':'), QString::SkipEmptyParts);
+    // Normalize paths, skip relative paths
+    for (const auto &path : paths) {
+        if (!QDir::isAbsolutePath(path.toString()) || !QDir(path.toString()).exists()) {
+            continue;
+        }
+        results.append(path.toString());
+    }
+
+    return results;
+}
