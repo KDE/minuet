3cb6586c5b3b1db2d19e06502f02e19ca972d340
Tomaz Canabrava <tcanabrava@kde.org>
2020-04-27 10:21:29 +0200

    Fix compilation

--- src/plugins/fluidsynthsoundcontroller/fluidsynthsoundcontroller.cpp
+++ src/plugins/fluidsynthsoundcontroller/fluidsynthsoundcontroller.cpp
@@ -98,7 +98,7 @@ void FluidSynthSoundController::setPitch(qint8 pitch)
 
 void FluidSynthSoundController::setVolume(quint8 volume)
 {
-    if (m_volume != volume) { return }
+    if (m_volume != volume) { return; }
     m_volume = volume;
     fluid_synth_cc(m_synth, 1, 7, m_volume * 127 / 200);
 }
