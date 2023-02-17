ebc1c37a1221f5f54a377ad4450d6cb5595a52c4
Albert Astals Cid <aacid@kde.org>
2023-02-17 12:20:52 +0100

    Fix extraction now that there's a json file in the root folder

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
-find data/ -name *.json | while read FILE; do cat $FILE | sed -n 's/"name"\s*:\s*"\(.*\)",*$/\1/p' | sed -n 's/\s*//p'  >> strings.txt; done
-find data/ -name *.json | while read FILE; do cat $FILE | sed -n 's/"userMessage"\s*:\s*"\(.*\)",/\1/p' | sed -n 's/\s*//p' >> strings.txt; done
+find data/ -name "*.json" | while read FILE; do cat $FILE | sed -n 's/"name"\s*:\s*"\(.*\)",*$/\1/p' | sed -n 's/\s*//p'  >> strings.txt; done
+find data/ -name "*.json" | while read FILE; do cat $FILE | sed -n 's/"userMessage"\s*:\s*"\(.*\)",/\1/p' | sed -n 's/\s*//p' >> strings.txt; done
 sort -u strings.txt | while read STR; do printf "i18nc(\"technical term, do you have a musician friend?\", \"$STR\")\n" >> rc.cpp; done
 $XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/minuet.pot
 rm -f strings.txt
