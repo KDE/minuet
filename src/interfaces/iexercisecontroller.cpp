afe93ca6ea7494df55c19056828e109ad1560a1e
Luigi Toscano <luigi.toscano@tiscali.it>
2025-03-09 22:49:39 +0100

    Remove Qt5 support
    Set the minimum Qt to 6, remove the usage of QT_MAJOR_VERSION,
    remove the Qt version checking which references very old releases,
    and adapt the CI (including enabling it for windows/Qt6).

--- src/interfaces/iexercisecontroller.cpp
+++ src/interfaces/iexercisecontroller.cpp
@@ -28,11 +28,7 @@ namespace Minuet
 {
 IExerciseController::IExerciseController(QObject *parent) : QObject(parent)
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    qmlRegisterInterface<IExerciseController>("IExerciseController");
-#else
     qRegisterMetaType<IExerciseController>();
-#endif
 }
 
 void IExerciseController::setCurrentExercise(QVariantMap currentExercise)
